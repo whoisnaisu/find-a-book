062fd2874a78d7ee35d244ef6987ae2a8f1b8377
naisu <napas14204@gmaill.com>
2022-11-23 14:19:58 +0700

    Merge remote-tracking branch 'origin/dev'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import React, { useState, useCallback } from 'react';
 import axios from "axios";
+import glass from "../src/assets/loupe-svgrepo-com.svg"
 
 function App() {
   const [result, setResult] = useState([]);
+  const [isError, setIsError] = useState(false)
 
   const debounce = (func) => {
     let timer;
@@ -18,37 +20,43 @@ function App() {
   };
 
   const handleSearchText = async (event) => {
-    const searchText = event.target.value
-    const endpoint = `https://www.googleapis.com/books/v1/volumes?q=${searchText}`
-    const response = await axios.get(endpoint)
-    setResult(response.data.items)
+    try {
+      const searchText = event.target.value
+      const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchText}`)
+      console.log(response.data.items);
+      setResult(response.data.items)
+    } catch (error) {
+      setIsError(!isError)
+      const reload = () => window.location.reload()
+      setTimeout(() => { reload() }, 1000)
+    }
   };
-
 
   const optimizedFn = useCallback(debounce(handleSearchText), []);
 
-
   return (
     <div className="App">
+      <div className="picture" onClick={() => { alert("Search the book you would like to know!") }} ><img className="magnifying-glass" src={glass} alt="magnifying glass" /></div>
       <div className="head"><h1>Find a Book</h1></div>
       <div className='all-box'>
         <input
           className="search-input"
           placeholder="search for your stuff here!"
           onChange={optimizedFn} />
-        <div><p>Search results:</p></div>
-        {result.map((item, index) => {
-          return (
-            <div className="results" key={index}>
-              <div className="list">
+        <div><p>Search results will show here:</p></div>
+        <div className="results" >
+          {!isError ? (result.map((item) => {
+            return (
+              <div className="item-list" key={item.id}>
                 <a className="item-link" href={item.volumeInfo.previewLink}>
-                  <h3> {item.volumeInfo.title}</h3>
+                  {item.volumeInfo.title} <span>by</span> {item.volumeInfo.authors} <p>(Published on: {item.volumeInfo.publishedDate || "no info available"})</p>
                 </a>
-              </div>
-            </div>)
-        })
-        }
+              </div>)
+          })
+          ) : <div><p>reloading...</p></div>}
+        </div>
       </div>
+
     </div>
   );
 };
