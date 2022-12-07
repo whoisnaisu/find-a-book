2dcda3d8256332230e0676a2560cea5653f2d00e
naisu <napas14204@gmail.com>
2022-12-07 23:41:08 +0700

    style: new animation for head

--- src/App.js
+++ src/App.js
@@ -37,7 +37,17 @@ function App() {
   return (
     <div className="App">
       <div className="picture" onClick={() => { alert("Search a book you would like to know!") }} ><img className="magnifying-glass" src={glass} alt="magnifying glass" /></div>
-      <div className="head"><h1>Find a Book</h1></div>
+      <div className="head">
+        <h1 className="f">F</h1>
+        <h1 className="i">i</h1>
+        <h1 className="n">n</h1>
+        <h1 className="d">d</h1>
+        <h1 className="a">&nbsp;a&nbsp;</h1>
+        <h1 className="b">B</h1>
+        <h1 className="o1">o</h1>
+        <h1 className="o2">o</h1>
+        <h1 className="k">k</h1>
+      </div>
       <div className='all-box'>
         <input
           className="search-input"
